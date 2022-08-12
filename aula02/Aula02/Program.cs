95043152a9db808021e05412f1b96c2375f4c32a
Carlos Henrique <annachav3s@gmail.com>
2022-08-11 21:56:03 -0300

    commit

--- aula02/Aula02/Program.cs
+++ aula02/Aula02/Program.cs
@@ -6,7 +6,23 @@ namespace Aula02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!\n");
+            //double area = CalculaArea(35, 3);
+           //Console.WriteLine(area);
+            Console.WriteLine("Informe uma largura: ");
+            double largura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe uma altura");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            double area = CalculaArea(largura, altura);
+            Console.WriteLine("A area equivale a: ");
+            Console.WriteLine(area);
+            
+        }
+
+        static double CalculaArea(double largura, double altura)
+        {
+            return  largura * altura;
+            
         }
     }
 }
