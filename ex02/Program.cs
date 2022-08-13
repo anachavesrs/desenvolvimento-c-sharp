38cc2c69b0637eeec5e7751b8aa22f5e53e3e650
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-08-13 12:38:44 -0300

    ex 2 conversão monetária

---
+++ ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex02 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double taxaDolar = 5.17;
+            double taxaEuro = 6.17;
+            double taxaPesoArgentino = 0.05;
+            double conversaoDolar = 0;
+            double ConversaoEuro = 0;
+            double ConversaoPesoArgentino = 0;
+
+            Console.WriteLine("Exercício 2 da lista");
+            Console.WriteLine("Algoritmo de conversão monetária em C#");
+            Console.WriteLine("Informe o valor em real: ");
+            double real = Convert.ToDouble(Console.ReadLine());
+
+            conversaoDolar = real / taxaDolar;
+            ConversaoEuro = real / taxaEuro;
+            ConversaoPesoArgentino = real / taxaPesoArgentino;
+
+            Console.WriteLine(" A conversão do valor informado em dólar é " + conversaoDolar + "\n Em euro é " + ConversaoEuro + "\n Em peso argentino é: " + ConversaoPesoArgentino);
+            
+
+        }
+    }
+}
