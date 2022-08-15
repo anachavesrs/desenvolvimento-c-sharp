348b2e540bfc9af5b1c3b388f49ebe931bce911d
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-08-15 20:58:39 -0300

    commit ex classifica idade

---
+++ ex04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex04 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+            Console.WriteLine("Informe sua idade");
+            int idade = Convert.ToInt16(Console.ReadLine());
+            string resposta ;
+
+            if (idade <= 13){
+                 resposta = "Criança";
+           } else if (idade <= 18){
+                 resposta = "Adolescente";
+           } else if (idade <= 60){
+                 resposta = "Adulto";
+           }else{
+                resposta = "Idoso";
+           }
+            Console.WriteLine("De acordo com a sua idade, você está classificado como: " + resposta  );
+            
+            }catch (Exception erro){
+                Console.WriteLine(erro.Message);
+            }
+        }
+
+    }
+}
