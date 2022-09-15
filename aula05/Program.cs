4a9821b5d0af4c8d3a4da781100af94da66ad19e
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-09-15 20:17:41 -0300

    commit

---
+++ aula05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace aula05 // Note: actual namespace depends on the project name.
+{
+     class Usuario
+    {
+
+            string username;
+            string hash;
+
+        public Usuario(string u, string p){
+            username = u;
+            ApplyHash(p);
+        }
+
+
+        void ApplyHash(string password){
+
+        //TODO aplicar hash
+      }
+
+
+      public string getUsername(){
+
+        return username;
+      }
+
+      public string setUsername(){
+
+        return hash;
+      }
+    }
+
+
+}
